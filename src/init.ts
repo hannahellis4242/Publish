1b8c9d4cc3bb28cbaf82bc7063fb96a8b599d817
Hannah Ellis <hannahellis4242@gmail.com>
2021-12-20 08:44:02 +0000

    adding example of how to make a directory

--- src/init.ts
+++ src/init.ts
@@ -1,3 +1,10 @@
+import { mkdir } from "fs";
+
 export const init = (title: string) => {
   console.log("TODO");
+  mkdir("./test/test", { recursive: true }, (err) => {
+    if (err) {
+      console.log("error : ", err.message);
+    }
+  });
 };
